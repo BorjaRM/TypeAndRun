f0d8c4dfd3b90e236e6d6b74bb4366cc204d50f5
borome <borome@A18C2.alumnado.flo>
2017-02-24 19:10:38 +0100

    Estructura principal

---
+++ src/TypeAndRun/Inicio.java
@@ -0,0 +1,12 @@
+package TypeAndRun;
+
+import Vistas.VentanaPrincipal;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+		VentanaPrincipal frame = new VentanaPrincipal();
+		frame.setVisible(true);
+	}
+
+}
